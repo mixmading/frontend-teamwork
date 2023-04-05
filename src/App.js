ed8b993f197746e31aac91c3be4a02ed96f1c5d1
mixmading <meri_lautkis@hotmail.com>
2023-04-05 14:17:27 +0300

    Siel on vittu välilehtii

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import './App.css';
-import { Tabs } from './Tabs';
-import { BrowserRouter } from 'react-router-dom';
+import Tabs from './Tabs';
 import { Header } from './components/Header';
-import { Cars } from './components/Cars';
-import { Home } from './components/Home';
-import { ElectricCars } from './components/ElectricCars';
-import { AllCars } from './components/AllCars';
-import { AllElectricCars } from './components/AllElectricCars';
-import { Routes, Route } from 'react-router-dom';
-import { NavBar } from './components/NavBar';
 
 function App() {
+  
   return (
-    <div>
+    <div className='App'>
       <Header></Header>
-      <Tabs></Tabs>
-      <NavBar>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/Home" element={<Home/>}/>
-            <Route path="/Cars" element={<Cars/>}/>
-            <Route path="/ElectricCars" element={<ElectricCars/>}/>
-            <Route path="/AllCars" element={<AllCars/>}/>
-            <Route path="/AllElectricCars" element={<AllElectricCars/>}/>
-          </Routes>
-        </BrowserRouter>
-      </NavBar>
+      <Tabs />
+      
     </div>
   );
 }
--- src/Tabs.js
+++ src/Tabs.js
@@ -1,54 +1,84 @@
 import { useState } from "react";
-import"./App.css";
+import "./App.css";
 
 function Tabs() {
-    
-    const [toggleState, setToggleState] = useState(1);
-        const toggleTab = () => {
-            console.log("Hello world");
-        }
+  const [toggleState, setToggleState] = useState(1);
 
-    return (
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
-        <div className="container">
+  return (
+    <div className="container">
+      <div className="bloc-tabs">
+        <button
+          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(1)}
+        >
+          Cars 
+        </button>
+        <button
+          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(2)}
+        >
+          All Cars
+        </button>
+        <button
+          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(3)}
+        >
+          Electric Cars
+        </button>
+        <button
+          className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(4)}
+        >
+          All E-Cars
+        </button>
+      </div>
 
-            <div className="bloc-tabs">
-                <div className="tabs active-tabs"
-                onClick={toggleTab}
-                >Tab 1</div>
-                <div className="tabs">Tab 2</div>
-                <div className="tabs">Tab 3</div>
-                <div className="tabs">Tab 4</div>
-            </div>
-            <div className="content-tabs">
-
-                <div className="content active-content">
-                    <h2>Content 1</h2>
-                    <hr />
-                    <p>
-                        Kakkea
-                    </p>
-                </div>
-
-                <div className="content">
-                <h2>Content w</h2>
-                <hr />
-                <p>
-                    Paskea
-                </p>
-                </div>
-
-                <div className="content">
-                <h2>Content w</h2>
-                <hr />
-                <p>
-                    Paskea
-                </p>
-                </div>
-
-            </div>
+      <div className="content-tabs">
+        <div
+          className={toggleState === 1 ? "content  active-content" : "content"}
+        >
+          <h2>Cars</h2>
+          <hr />
+          <p>
+            Persettä
+          </p>
         </div>
-    );
+
+        <div
+          className={toggleState === 2 ? "content  active-content" : "content"}
+        >
+          <h2>All Cars</h2>
+          <hr />
+          <p>
+            Paskea
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 3 ? "content  active-content" : "content"}
+        >
+          <h2>Electric Cars</h2>
+          <hr />
+          <p>
+            Kakkea
+          </p>
+        </div>
+        <div
+          className={toggleState === 4 ? "content  active-content" : "content"}
+        >
+          <h2>All Electric Cars</h2>
+          <hr />
+          <p>
+            Ribals
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Tabs;

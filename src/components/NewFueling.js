f328e9faba0651de75701382d3975d4ee28d9547
areocentric <117666021+areocentric@users.noreply.github.com>
2023-04-12 21:45:07 +0300

    Lisätty max input character amount

--- src/components/NewFueling.js
+++ src/components/NewFueling.js
@@ -1,78 +1,79 @@
-import React, {useState, useContext} from 'react'
+import React, { useState, useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const NewFueling = () => {
-    const [text, setText] =useState('');
-    const [Euro, setEuro] =useState('');
-    const [Litre, setLitre] =useState('');
-    const [kWh, setkWh] =useState('');
-    const [km, setkm]  =useState('');
+  const [text, setText] = useState('');
+  const [Euro, setEuro] = useState('');
+  const [Litre, setLitre] = useState('');
+  const [kWh, setkWh] = useState('');
+  const [km, setkm] = useState('');
 
-    const { addTransaction } = useContext(GlobalContext);
+  const { addTransaction } = useContext(GlobalContext);
 
-    const onSubmit = e => {
-      e.preventDefault();
+  const onSubmit = e => {
+    e.preventDefault();
 
-      const newTransaction = {
-        id: Math.floor(Math.random() * 100000000),
-        text,
-        Euro: +Euro,
-        Litre: +Litre,
-        kWh: +kWh,
-        km: +km
-      }
-
-      addTransaction(newTransaction);
-      // Clear input fields
-      setText('');
-      setEuro('');
-      setLitre('');
-      setkWh('');
-      setkm('');
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text: text.substring(0, 20),
+      Euro: +Euro.substring(0, 4),
+      Litre: +Litre.substring(0, 4),
+      kWh: +kWh.substring(0, 4),
+      km: +km.substring(0, 4)
     }
+
+    addTransaction(newTransaction);
+    // Clear input fields
+    setText('');
+    setEuro('');
+    setLitre('');
+    setkWh('');
+    setkm('');
+  }
+
   return (
     <>
-    <h3>Add New Fueling</h3>
-    <form onSubmit={onSubmit}>
+      <h3>Add New Fueling</h3>
+      <form onSubmit={onSubmit}>
 
-      <div className="form-control">
-        <label htmlFor="text">Car type</label><br />
-        <input type="radio" id="petrol" name="car_type" value="Petrol" checked />
-        <label htmlFor="petrol" style={{marginRight: '20px'}}>Petrol</label>
-        <input type="radio" id="electric" name="car_type" value="Electric" />
-        <label htmlFor="electric">Electric</label>
-      </div>
+        <div className="form-control">
+          <label htmlFor="text">Car type</label><br />
+          <input type="radio" id="petrol" name="car_type" value="Petrol" checked />
+          <label htmlFor="petrol" style={{ marginRight: '20px' }}>Petrol</label>
+          <input type="radio" id="electric" name="car_type" value="Electric" />
+          <label htmlFor="electric">Electric</label>
+        </div>
 
-      <div className="form-control">
-        <label htmlFor="text">Car name</label>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} 
-        placeholder="Enter car name..." />
-      </div>
+        <div className="form-control">
+          <label htmlFor="text">Car name (max 20 characters)</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value.substring(0, 20))}
+            placeholder="Enter car name..." maxLength={20} />
+        </div>
 
-      <div className="form-control">
-        <label htmlFor="amount-euro">Cost €</label>
-        <input type="number" value={Euro} onChange={(e) => setEuro(e.target.value)}
-        placeholder="Enter price in €..." />
-      </div>
-    
-      <div className="form-control">
-        <label htmlFor="amount-litre">Litre amount</label>
-        <input type="number" value={Litre} onChange={(e) => setLitre(e.target.value)}
-        placeholder="Enter the amount of fuel in litres..." />
-      </div>
+        <div className="form-control">
+          <label htmlFor="amount-euro">Cost € (max 4 characters)</label>
+          <input type="number" value={Euro} onChange={(e) => setEuro(e.target.value.substring(0, 4))}
+            placeholder="Enter price in €..." maxLength={4} />
+        </div>
 
-      <div className="form-control">
-        <label htmlFor="amount-kWh">Amount charged</label>
-        <input type="number" value={kWh} onChange={(e) => setkWh(e.target.value)}
-        placeholder="Enter charging amount in kWh ..." />
-      </div>
+        <div className="form-control">
+          <label htmlFor="amount-litre">Litre amount (max 4 characters)</label>
+          <input type="number" value={Litre} onChange={(e) => setLitre(e.target.value.substring(0, 4))}
+            placeholder="Enter the amount of fuel in litres..." maxLength={4} />
+        </div>
 
-      <div className="form-control">
-        <label htmlFor="amount-km">Amount driven</label>
-        <input type="number" value={km} onChange={(e) => setkm(e.target.value)}
-        placeholder="Enter km amount driven between fuelings..." />
-      </div>
-      <button className="btn">Submit</button>
+        <div className="form-control">
+          <label htmlFor="amount-kWh">Amount charged (max 4 characters)</label>
+          <input type="number" value={kWh} onChange={(e) => setkWh(e.target.value.substring(0, 4))}
+            placeholder="Enter charging amount in kWh ..." maxLength={4} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="amount-km">Amount driven (max 4 characters)</label>
+          <input type="number" value={km} onChange={(e) => setkm(e.target.value.substring(0, 4))}
+            placeholder="Enter km amount driven between fuelings..." maxLength={4} />
+        </div>
+        <button className="btn">Submit</button>
     </form>
     </>
     )

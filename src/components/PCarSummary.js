b796cf0e0b22139fe20dff7b62ed471f120f0a78
Oskari Lehto <112509879+leht0@users.noreply.github.com>
2023-04-20 10:13:24 +0300

    PcarSummary
    Petrol autojen summa

---
+++ src/components/PCarSummary.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+function PCarSummary() {
+  const { transactions } = useContext(GlobalContext);
+
+  // Calculate the summary of all petrol car transactions
+  const petrolTransactions = transactions.filter(
+    (transaction) => transaction.type_id === "petrol"
+  );
+  const euroTotal = petrolTransactions.reduce(
+    (acc, transaction) => acc + transaction.Euro,
+    0
+  );
+  const litreTotal = petrolTransactions.reduce(
+    (acc, transaction) => acc + transaction.Litre,
+    0
+  );
+
+  return (
+    <div className="content  active-content">
+      <h2>Summary of all petrol car transactions</h2>
+      <hr />
+      <p>Total €: {euroTotal.toFixed(2)}</p>
+      <p>Total litres: {litreTotal.toFixed(2)}</p>
+    </div>
+  );
+}
+
+export default PCarSummary;
